4909642ea7339d9e098656a73777ac1c9631f043
Shrenik Balaji Sirimilla <137373497+Shrenik1713@users.noreply.github.com>
2025-06-13 23:30:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
